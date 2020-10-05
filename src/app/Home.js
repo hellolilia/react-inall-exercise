28508609c8101ea9b39f13d1e35729456ca68320
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-05 16:53:16 +0800

    finished home page

--- src/app/Home.js
+++ src/app/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.less';
 import calculatorImage from "../images/calculator.png"
 import timerImage from "../images/timer.png"
+import {Link} from "react-router-dom";
 
 const Home = () => {
   return (
@@ -11,6 +12,10 @@ const Home = () => {
           <div className="grayBox" />
           <img className="calculatorImage" src={calculatorImage} alt="calculatorImage"/>
           <img className="timerImage" src={timerImage} alt="timerImage"/>
+          <p className="nav">
+              <Link to='/calculator'>计算器</Link>
+              <Link to='/timer'>倒计时器</Link>
+          </p>
       </div>
   );
 };
