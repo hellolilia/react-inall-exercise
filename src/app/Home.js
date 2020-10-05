e622427d7437c29b934e0e6d702b44acc908dcb7
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-05 16:25:12 +0800

    add gray box

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './home.less';
+
 const Home = () => {
   return (
       <div className="home">
         <div className = "heroImage" />
         <h1>在线实用工具</h1>
+          <div className="grayBox" />
+
       </div>
   );
 };
