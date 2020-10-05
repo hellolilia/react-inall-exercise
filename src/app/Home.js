388f67cc249de1dc1a9e5c651117ed1471c23c0e
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-05 16:14:17 +0800

    add hero image

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './home.less';
-
 const Home = () => {
-  return (<div className="home">
-    This is home page
-  </div>);
+  return (
+      <div className="home">
+        <div className = "heroImage" />
+        <h1>在线实用工具</h1>
+      </div>
+  );
 };
 
 export default Home;
