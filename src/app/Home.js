c7d304362b9907810a25a02937a0a8c1509cc5b3
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-05 16:44:58 +0800

    add timer and calculator image

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './home.less';
+import calculatorImage from "../images/calculator.png"
+import timerImage from "../images/timer.png"
 
 const Home = () => {
   return (
       <div className="home">
-        <div className = "heroImage" />
-        <h1>在线实用工具</h1>
+          <div className = "heroImage" />
+          <h1>在线实用工具</h1>
           <div className="grayBox" />
-
+          <img className="calculatorImage" src={calculatorImage} alt="calculatorImage"/>
+          <img className="timerImage" src={timerImage} alt="timerImage"/>
       </div>
   );
 };
